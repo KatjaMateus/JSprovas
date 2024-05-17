f10fd9324c24dabf01f8de8fe1b15d032bbb91ed
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2024-05-17 17:12:08 -0300

    Add files via upload

---
+++ aula1_prova.js
@@ -0,0 +1,8 @@
+const n1 = 7
+const n2 = 8
+const n3 = 8.5
+const p1 = 1
+const p2 = 2
+const p3 = 3
+const media = ((n1 * p1)+(n2 * p2)+(n3 * p3)) / 6
+console.log(`A média das notas é ${media.toFixed(1)}.`)
