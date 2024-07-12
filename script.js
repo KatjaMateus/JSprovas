5a2d5312b53fbfe9280799723ad6e7fe3688ff1c
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2024-07-12 18:59:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const formulario = document.querySelector("#formulario")
+const tarefa = document.querySelector("#tarefa")
+
+const resultado = document.querySelector("#resultado")
+
+
+formulario.addEventListener("submit", (evento) =>{
+    evento.preventDefault()
+    
+    const nova_tarefa = document.createElement("li")
+
+    const span = document.createElement("span")
+    span.textContent = tarefa.value
+
+
+    
+    const input = document.createElement("input")
+    input.type = "checkbox"
+
+    input.addEventListener("click", ()=>{
+        if(input.checked) {
+            span.classList.add("riscado")
+        }else{
+            span.classList.remove("riscado")
+        }
+    })
+  
+
+    const button = document.createElement("button")
+    button.className="interno"
+    button.textContent = "Excluir"
+
+    button.addEventListener("click", ()=>{
+        resultado.removeChild(nova_tarefa)
+
+    })
+
+    nova_tarefa.append(input, span,  button)
+
+
+    resultado.appendChild(nova_tarefa)
+
+    tarefa.value = ""
+    tarefa.focus()
+
+})
